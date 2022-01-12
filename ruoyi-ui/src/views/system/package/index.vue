a0687ea88d53c2dfdb3b3f1a9a2b535cc7a62339
CAI XIN <x.a.cai@accenture.com>
2022-01-12 14:44:45 +0800

    update

--- ruoyi-ui/src/views/system/package/index.vue
+++ ruoyi-ui/src/views/system/package/index.vue
@@ -190,13 +190,6 @@
             v-hasPermi="['system:package:remove']"
           >删除</el-button>
           -->
-           <el-button
-             size="mini"
-             type="text"
-             icon="el-icon-edit"
-             @click="handleUpdate(scope.row)"
-             v-hasPermi="['system:package:edit']"
-           >修改</el-button>
           <el-button
             size="mini"
             type="text"
