865d7e623d1928c0d0208f0362b45af18af62225
CAIXIN <1097700731>
2023-08-02 07:05:47 +0800

    转寄修改

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SyncHistoryDpdMapper.java
@@ -0,0 +1,63 @@
+package com.ruoyi.system.mapper;
+
+import java.util.List;
+import com.ruoyi.system.domain.SyncHistoryDpd;
+
+/**
+ * 同步历史记录Mapper接口
+ * 
+ * @author ruoyi
+ * @date 2023-08-02
+ */
+public interface SyncHistoryDpdMapper 
+{
+    /**
+     * 查询同步历史记录
+     * 
+     * @param id 同步历史记录主键
+     * @return 同步历史记录
+     */
+    public SyncHistoryDpd selectSyncHistoryDpdById(Long id);
+
+    /**
+     * 查询同步历史记录列表
+     * 
+     * @param syncHistoryDpd 同步历史记录
+     * @return 同步历史记录集合
+     */
+    public List<SyncHistoryDpd> selectSyncHistoryDpdList(SyncHistoryDpd syncHistoryDpd);
+
+    public List<SyncHistoryDpd> selectNoNeedSyncHistoryDpdList();
+
+    /**
+     * 新增同步历史记录
+     * 
+     * @param syncHistoryDpd 同步历史记录
+     * @return 结果
+     */
+    public int insertSyncHistoryDpd(SyncHistoryDpd syncHistoryDpd);
+
+    /**
+     * 修改同步历史记录
+     * 
+     * @param syncHistoryDpd 同步历史记录
+     * @return 结果
+     */
+    public int updateSyncHistoryDpd(SyncHistoryDpd syncHistoryDpd);
+
+    /**
+     * 删除同步历史记录
+     * 
+     * @param id 同步历史记录主键
+     * @return 结果
+     */
+    public int deleteSyncHistoryDpdById(Long id);
+
+    /**
+     * 批量删除同步历史记录
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteSyncHistoryDpdByIds(Long[] ids);
+}
--- sql/step_update_2023073101.sql
+++ sql/step_update_2023073101.sql
@@ -1,16 +1,16 @@
-drop table if exists sync_dpd_history;
-create table sync_dpd_history
+drop table if exists sync_history_dpd;
+create table sync_history_dpd
 (
-    id          bigint(20) not null auto_increment comment '用户ID',
+    id          bigint(20) not null auto_increment comment '主键',
     bat_id      bigint(20) default null comment '批量任务历史Id',
     message_id varchar(500) default null comment '信息id',
     file_name   varchar(500) not null comment '文件名称',
     sync_status char(1)      default '0' comment '同步状态 1-同步成功 0-同步失败 2-部分成功',
     file_delete_status   char(1)      default '0' comment '文件是否删除 0-未删除 1-已经删除',
     error_msg   varchar(500) default null comment '错误信息',
-    sync_time   datetime     default sysdate() comment '同步时间',
+    sync_time   datetime     default now() comment '同步时间',
     remark      varchar(500) default null comment '备注',
-    primary key (user_id)
+    primary key (id)
 ) engine=innodb CHARSET=utf8  auto_increment=100 comment = '同步历史记录表';
 
 
