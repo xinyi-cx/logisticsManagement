ae2f6928b92a78278e4eebdf2553ab0dc5092717
x.a.cai <x.a.cai@accenture.com>
2023-08-01 18:52:56 +0800

    [x.a.cai] #OTR-19341 delete unused

--- sql/step_update_2023073101.sql
+++ sql/step_update_2023073101.sql
@@ -14,5 +14,10 @@ create table sync_dpd_history
 ) engine=innodb CHARSET=utf8  auto_increment=100 comment = '同步历史记录表';
 
 
-insert into sys_config values(11, '同步DPD信息路径',         'sys.sync.dpd.path',         'C:/ruoyi/uploadPath/packagePdf/',        'Y', 'admin', sysdate(), '', null, '同步DPD信息路径' );
+insert into sys_config values(11, '同步DPD信息路径-local-pl',         'sys.sync.dpd.local.pl.path',         'C:/ruoyi/uploadPath/local/pl',        'Y', 'admin', sysdate(), '', null, '同步DPD信息路径 local-pl' );
+insert into sys_config values(12, '同步DPD信息路径-local-cz',         'sys.sync.dpd.local.cz.path',        'C:/ruoyi/uploadPath/local/cz',       'Y', 'admin', sysdate(), '', null, '同步DPD信息路径 local-cz' );
+insert into sys_config values(13, '同步DPD信息路径-转寄-pl',         'sys.sync.dpd.ref.pl.path',         'C:/ruoyi/uploadPath/ref/pl',      'Y', 'admin', sysdate(), '', null, '同步DPD信息路径 转寄-pl' );
+insert into sys_config values(14, '同步DPD信息路径-转寄-cz',         'sys.sync.dpd.ref.cz.path',         'C:/ruoyi/uploadPath/ref/cz',       'Y', 'admin', sysdate(), '', null, '同步DPD信息路径 转寄-cz' );
+insert into sys_config values(15, '同步DPD信息路径-面单-pl',         'sys.sync.dpd.original.pl.path',       'C:/ruoyi/uploadPath/original/pl',         'Y', 'admin', sysdate(), '', null, '同步DPD信息路径 面单-pl' );
+insert into sys_config values(16, '同步DPD信息路径-面单-cz',         'sys.sync.dpd.original.cz.path',         'C:/ruoyi/uploadPath/original/cz',         'Y', 'admin', sysdate(), '', null, '同步DPD信息路径 面单-cz' );
 
